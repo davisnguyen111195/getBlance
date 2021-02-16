b581df6a4cb6590ca17b15b095488b760323f24b
DavisNguyen <nguyendat111195@gmail.com>
2021-02-16 14:03:35 +0700

    add code

---
+++ index.js
@@ -0,0 +1,26 @@
+const rp = require("request-promise")
+const cheerio = require("cheerio")
+const fs = require("fs")
+let address = `AJzoeKrj7RHMwSrPQDPdv61ciVEYpmhkjk`
+const URL = `https://neoscan.io/address/` + address;
+ 
+const options = {
+  uri: URL,
+  transform: function (body) {
+    //Khi lấy dữ liệu từ trang thành công nó sẽ tự động parse DOM
+    return cheerio.load(body);
+  },
+};
+ 
+(async function crawler() {
+  try {
+    // Lấy dữ liệu từ trang crawl đã được parseDOM
+    var $ = await rp(options);
+  } catch (error) {
+    return error;
+  }
+
+  let neoBalance = $("balance-amount").text().trim();
+  fs.writeFileSync('data.json', JSON.stringify(data))
+
+})
